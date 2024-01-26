aa626580e60b27d4ceecbcce72d3dd473fd02ac8
Jigmet Dadul <85403349+jigmetdadul@users.noreply.github.com>
2024-01-26 17:53:17 +0530

    Initial Commit

---
+++ rockPaperScissor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rockPaperScissor
+//
+//  Created by Jigmet stanzin Dadul on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rockPaperScissor/rockPaperScissorApp.swift
@@ -0,0 +1,17 @@
+//
+//  rockPaperScissorApp.swift
+//  rockPaperScissor
+//
+//  Created by Jigmet stanzin Dadul on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct rockPaperScissorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
